63308377ced488081c3f84166fcfce0c273a14f5
ZX <71930531+Xiaoxue-xx@users.noreply.github.com>
2023-01-13 16:41:38 +0800

    Update sampler.py

--- trainer/sampler.py
+++ trainer/sampler.py
@@ -1,9 +1,13 @@
 import random
 
+from arguments import DataArguments
+
 
 class RandomSampleNegatives:
-    def __init__(self, config):
-        self.num_negative = config.num_negative
+    def __init__(self, data_args: DataArguments):
+        self.num_negative = data_args.train_n_passages - 1
+        self.q_max_len = data_args.q_max_len
+        self.p_max_len = data_args.p_max_len
 
     def __call__(self, samples):
         queries = []
@@ -11,15 +15,16 @@ class RandomSampleNegatives:
         for sample in samples:
             document = []
             queries.append(sample['query'])
-            document.append(sample['positives'])
+            document.append(random.choices(sample['positives'])[0])
             all_negative_num = len(sample['negatives'])
             assert all_negative_num >= self.num_negative, \
                 f"The num_negative {self.num_negative} should be no larger than all_negative_num {all_negative_num}."
-            cand_doc = range(all_negative_num)
+            cand_doc = list(range(all_negative_num))
             random.shuffle(cand_doc)
             cand_doc = cand_doc[:self.num_negative]
             for idx in cand_doc:
                 document.append(sample['negatives'][idx])
             documents.append(document)
-        return {'query': queries}, {'passage': documents}
+
+        return queries, documents
 
